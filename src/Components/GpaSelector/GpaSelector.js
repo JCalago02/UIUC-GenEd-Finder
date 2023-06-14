867e54eb1b863619e2ef48c441520d213250a0ad
JCalago02 <jcalago02@gmail.com>
2023-06-13 23:21:45 -0500

    Create state for GPA selector widget

--- src/Components/GpaSelector/GpaSelector.js
+++ src/Components/GpaSelector/GpaSelector.js
@@ -1,10 +1,37 @@
 import './GpaSelector.css'
-function GpaSelector(setMinGPA) {
+import { useState } from 'react'
+
+
+function GpaSelector({setGPABounds}) {
+    const [localGPABounds, setLocalGPABounds] = useState([-1, -1]);
+
+    function HandleMinChange(e) {
+        const isValid = e.target.value.length !== 0;
+        if (isValid) {
+            setLocalGPABounds([e.target.value, localGPABounds[1]]);
+        } else {
+            setLocalGPABounds([-1, localGPABounds[1]]);
+        }
+    }
+
+    function HandleMaxChange(e) {
+        const isValid = e.target.value.length !== 0;
+        if (isValid) {
+            setLocalGPABounds([localGPABounds[1], e.target.value]);
+        } else {
+            setLocalGPABounds([localGPABounds[1], -1]);
+        }
+    }
+
+    function HandleSubmit() {
+        setGPABounds(localGPABounds);
+    }
+
     return (
         <div className="flex-wrapper-gpa">
-             <input type="number" className="deci-input" step={0.1} placeholder='Min'></input>
-            <input type="number" className="deci-input" step={0.1} placeholder='Max'></input>
-            <button className='submit'>Submit</button>
+            <input type="number" className="deci-input" step={0.1} placeholder='Min' onChange={HandleMinChange} ></input>
+            <input type="number" className="deci-input" step={0.1} placeholder='Max' onChange={HandleMaxChange}></input>
+            <button className='submit' onClick={HandleSubmit}>Submit</button>
         </div>
     )
 }
