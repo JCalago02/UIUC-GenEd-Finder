eff3aa45627421ef15ba8be386435f5337dcc070
JCalago02 <jcalago02@gmail.com>
2023-06-13 23:20:28 -0500

    Add routing

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,16 @@
-import './Header.css'
-
+import './Header.css';
+import { Link } from 'react-router-dom';
 function Header() {
     return (
-        <div className="headerWrapper">
-            UIUC GenEd Finder
+        <div className="header-background-div">
+            <section className="header-content">
+                <Link to="/">UIUC GenEd Finder</Link>
+                <Link to="find-classes">Find Classes</Link>
+            </section>
         </div>
     )
 }
 
 export default Header
+
+
