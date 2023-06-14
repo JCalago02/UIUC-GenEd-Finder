fcaafb18b4ad1fa57c9ea64f8fbd8ad6ad55b439
JCalago02 <jcalago02@gmail.com>
2023-06-13 23:19:56 -0500

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import './App.css';
 import Header from './Components/Header/Header';
-import SearchFilterRow from './Sections/SearchFilterRow/SearchFilterRow';
-import RestrictionList from './Sections/RestrictionList/RestrictionList'
-import { useState } from 'react';
-function App() {
-  const [restrictionList, setRestrictionList] = useState(["Hello", "There"]);
+import FindClasses from './Pages/FindClasses/FindClasses';
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+
+
+
+const NotImplemented = () => {
   return (
-    <div className="App">
-      <Header></Header>
-      <SearchFilterRow/>
-      <RestrictionList restrictionList={restrictionList} setRestrictionList={setRestrictionList}></RestrictionList>
-    </div>
+    <div>Not implemented lmfao</div>
+  )
+}
+
+function Root() {
+  return (
+    <>
+      <Header/>
+      <Outlet/>
+    </>
+  )
+  
+}
+ 
+
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root/>}>
+        <Route index element={<NotImplemented/>}></Route>
+        <Route path="/find-classes" element={<FindClasses/>}/>
+      </Route>
+      
+    )
+  )
+  return (
+      <div className="App">
+        <RouterProvider router={router}></RouterProvider> 
+      </div>
   );
 }
 
