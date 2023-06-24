d32dc2afa6bc0a576fd51b118b5a0f0750301bb8
JCalago02 <jcalago02@gmail.com>
2023-06-24 18:30:57 -0500

    Add header navigation links and formatting

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,8 +4,14 @@ function Header() {
     return (
         <div className="header-background-div">
             <section className="header-content">
-                <Link to="/">UIUC GenEd Finder</Link>
-                <Link to="find-classes">Find Classes</Link>
+                <Link className='header-link main-link' to="/">UIUC GenEd Finder</Link>
+                <div className="right-align-links">
+                    <Link className='header-link' to="about">About</Link>
+                    <Link className='header-link' to="find-classes">Find Classes</Link>
+                    <Link className='header-link' to="share">Share</Link>
+                    <Link className='header-link' to="login">Login</Link>
+                </div>
+                
             </section>
         </div>
     )
